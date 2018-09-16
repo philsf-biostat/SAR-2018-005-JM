aa649bd4d3f81b7569d5fb872670b6623a8340fb
Felipe Figueiredo <philsf79@gmail.com>
2018-09-16 18:38:29 -0300

    título da figura: monoplex

--- scripts/visualizations.R
+++ scripts/visualizations.R
@@ -6,7 +6,7 @@ ggplot(virs, aes(Sample, Quantity)) +
   geom_point(alpha = .25) +
   xlab("") + ylab("Copies/PCR (log10)") +
   ylim(c(0,10)) +
-  # ggtitle("Potency") +
+  ggtitle("Monoplex") +
   # theme(legend.position = "bottom", legend.title = element_blank()) +
   facet_wrap(~ Virus)
 ggsave("figures/potency.png", width = 7, height = 7)
